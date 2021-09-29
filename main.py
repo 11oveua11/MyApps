eea11e8d368037fff143481a25e6afb2a4bc98e7
11oveua11 <khost@bk.ru>
2021-09-29 19:29:01 +0300

    2021.01 + halfwork

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from shutil import copy
 from datetime import date, timedelta
 from ctypes import *
+from os import listdir
+from os.path import isfile, join
+
 windll.Kernel32.GetStdHandle.restype = c_ulong
 h = windll.Kernel32.GetStdHandle(c_ulong(0xfffffff5))
-for color in range(16):
-    setclr = windll.Kernel32.SetConsoleTextAttribute
-    setclr(h, color)
-
+setclr = windll.Kernel32.SetConsoleTextAttribute
 
 def fnc(days_ago):
     '''File Name Constructor.
@@ -19,12 +19,11 @@ def wfc(fn):
     '''Was file copied?
     Проверка, был ли файл ранее скопирован?
     1 аргумент - fn - имя файла'''
-    with open('copied.txt') as copied:
-        lst_of_files = [line for line in copied]
-        if fn in lst_of_files:
-            return True
-        else:
-            return False
+    with open('copied.txt', 'r') as copied:
+        for line in copied:
+            if fn == line.strip():
+                return True
+        return False
 
 def copy_day(fn):
     'копиирование файла'
@@ -36,19 +35,33 @@ def copy_day(fn):
             setclr(h, 14)
             print("попытка скопировать: "+fn+", жди...\nИЗ: "+src+"\nВ: "+dst)
             copy(src + fn, dst + fn)
+            setclr(h, 10)
             print("СКОПИРОВАЛ "+fn)
             with open('copied.txt', 'a') as f:
                 f.write(fn+'\n')
         except:
+            setclr(h, 12)
             print('!НЕ СКОПИРОВАЛОСЬ :-(')
+def check_last_week():
+    onlyfiles = [f for f in listdir(src) if isfile(join(src, f))]
+    print(onlyfiles)
+    print(len(onlyfiles))
 
-#src = '\\\\10.149.105.2\\data\\in\\'
-#dst = '\\\\Bam\D\\Bill\\Bill\\'
-src = 'c:\\Users\\Adm\\Desktop\\'
-dst = 'd:\\'
 
-copy_day(fnc(1))
+
+#src = '\\\\Bam\\D$\\Bill\\Bill\\'
+#dst = '\\\\10.149.105.2\\data\\in\\'
+archive = '\\\\10.149.105.2\\data\\archive\\'
+src = 'D:\\'
+dst = 'D:\I\\'
+
+setclr(h, 7)
+#copy_day(fnc(1))
+
+check_last_week()
 
 while True:
+
     if input() == 'exit':
         break
+setclr(h, 7)
