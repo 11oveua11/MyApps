d08fe4fcab1a789f14e2f8c9b8a2455ce55f8d42
11oveua11 <khost@bk.ru>
2021-09-30 01:33:38 +0300

    2021.02 release expanded algorithm

--- main.py
+++ main.py
@@ -8,28 +8,32 @@ windll.Kernel32.GetStdHandle.restype = c_ulong
 h = windll.Kernel32.GetStdHandle(c_ulong(0xfffffff5))
 setclr = windll.Kernel32.SetConsoleTextAttribute
 
+#def wfc(fn):
+#    '''Was file copied?
+#    Проверка, был ли файл ранее скопирован?
+#    1 аргумент - fn - имя файла'''
+#    with open('copied.txt', 'r') as copied:
+#        for line in copied:
+#            if fn == line.strip():
+#                return True
+#        return False
+
+def check_archive():
+    return [f[29:37]+'.BIL' for f in listdir(src) if isfile(join(src, f))]
+
 def fnc(days_ago):
     '''File Name Constructor.
     Коструктор имени файла.
     1 аргумент - days_ago - количество прошедших дней'''
-    file = (date.today() - timedelta(days=days_ago)).strftime('20%y%m%d.BIL')
-    return file
-
-def wfc(fn):
-    '''Was file copied?
-    Проверка, был ли файл ранее скопирован?
-    1 аргумент - fn - имя файла'''
-    with open('copied.txt', 'r') as copied:
-        for line in copied:
-            if fn == line.strip():
-                return True
-        return False
+    filename = (date.today() - timedelta(days=days_ago)).strftime('20%y%m%d.BIL')
+    return filename
 
 def copy_day(fn):
     'копиирование файла'
-    if wfc(fn):
+    if fn in all_copied_files:
         setclr(h, 11)
-        print('файл '+fn+' уже копировал')
+        print('файл '+fn+' уже найден в архиве')
+        return True
     else:
         try:
             setclr(h, 14)
@@ -37,16 +41,11 @@ def copy_day(fn):
             copy(src + fn, dst + fn)
             setclr(h, 10)
             print("СКОПИРОВАЛ "+fn)
-            with open('copied.txt', 'a') as f:
-                f.write(fn+'\n')
+            return True
         except:
             setclr(h, 12)
             print('!НЕ СКОПИРОВАЛОСЬ :-(')
-def check_last_week():
-    onlyfiles = [f for f in listdir(src) if isfile(join(src, f))]
-    print(onlyfiles)
-    print(len(onlyfiles))
-
+            return False
 
 
 #src = '\\\\Bam\\D$\\Bill\\Bill\\'
@@ -56,9 +55,15 @@ src = 'D:\\'
 dst = 'D:\I\\'
 
 setclr(h, 7)
-#copy_day(fnc(1))
 
-check_last_week()
+all_copied_files = check_archive()
+
+if all(copy_day(fnc(i)) for i in range(8, 1, -1)): #попытка скопировать последнюю неделю
+    setclr(h, 10)
+    print('Файлы за последнюю неделю были проверены. Всё ОК')
+else:
+    setclr(h, 12)
+    print('Файлы за последнюю неделю были проверены. Внимание, не все файлы есть в архиве.')
 
 while True:
 
